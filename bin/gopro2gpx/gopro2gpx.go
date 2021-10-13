520fc17aaa3f0ca0df61111019688d36060b0162
Shinichi Tanaka <shinichi.tnk@gmail.com>
2021-10-13 18:33:33 +0900

    Update gopro2gpx.go

--- bin/gopro2gpx/gopro2gpx.go
+++ bin/gopro2gpx/gopro2gpx.go
@@ -7,7 +7,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/stilldavid/gopro-utils/telemetry"
+//	"github.com/stilldavid/gopro-utils/telemetry"
+	"../../telemetry"
 	"github.com/tkrajina/gpxgo/gpx"
 )
 
